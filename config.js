0ed469a9963402b226d7705730041e332af9afc7
Cuong06 <86759226+Cuong06@users.noreply.github.com>
2021-07-01 11:12:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'hải hoà à cháu iu chú!',
+    introTitle: 'heyy bae sea!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
