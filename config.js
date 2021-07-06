ad358caceb2812d4440e888b3164c139e2db04d1
Cuong06 <86759226+Cuong06@users.noreply.github.com>
2021-07-06 20:07:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'con lợn đất kiaa :3!',
+    introTitle: 'ê con lợn Tranggg!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hehe',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: bao nhiêu cân rồiii 😙',
+    desc: '=)) ',
+    btnYes: 'thôi đành nói vậy',
+    btnNo: 'gầy à',
+    question:'nghỉ ở dịch béo lên à',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'tao có hơn 60 cân chứ mấy',
+    mess: 'oke t biết màaa',
+    messDesc: 'ăn ít thôi nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
